417d736f64d03c27b46116023c0e5e1b0a1edd74
Naveen chandta <nc.upadhyay20@gmail.com>
2023-01-14 22:08:07 +0530

    shagun :: initial commit

---
+++ app/src/main/java/com/nc/shagun/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nc.shagun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
